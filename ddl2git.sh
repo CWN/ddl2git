2e386056b2e13bf79bfd03643bc9a152779f00d0
CWN <32759+CWN@users.noreply.github.com>
2018-01-22 17:24:08 +0300

    Get oracle instance name and create destination directory if needed

--- ddl2git.sh
+++ ddl2git.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #************************************************************************************************#
+#                                                                                                #
 #                                   ddl2git.sh                                                   #
-#                     written by Denis Ryazanov aka CWN                                          #
-#                                     2011-2018                                                  #
+#                      written by Denis Ryazanov aka CWN                                         #
+#                                   2011-2018                                                    #
+#                        https://github.com/CWN/ddl2git                                          #
 #                                                                                                #
 #************************************************************************************************#
 
@@ -17,7 +19,10 @@ E_ERROR=10
 E_WRONG_ARGS=11
 E_CONNECTION_ERROR=12
 E_CANT_GET_USERS=13
+E_USERS_NOT_FOUND=14
+E_DESTINATION_DIR=15
 
+# check arguments
 if [ $# -ne "$ARGS_COUNT" ]; then
     echo -e "Usage: $SCRIPT_NAME oracle_connect_string"
     echo -e "\nExample: $SCRIPT_NAME user/password@tns_db_name"
@@ -34,11 +39,24 @@ function checkExitCode(){
 
     if [ "$exitCode" -ne 0 ]
     then
-      echo -e "SqlPlus error:\n${errorMsg}"
-      exit $errorCode
+        echo -e "SqlPlus error:\n${errorMsg}"
+        exit $errorCode
     fi
 }
 
+function checkAndCreateDestination(){
+    local dest=$1
+    if [ -z "${dest}" ]
+    then
+        echo "Destination directory not provided"
+        exit $E_DESTINATION_DIR
+    fi
+
+    if [ ! -e "$dest" ]
+    then
+        mkdir $dest
+    fi
+}
 
 # test DB connection
 CONNECTION_TEST=$(sqlplus -silent /nolog <<SQL
@@ -48,11 +66,14 @@ exit
 SQL
 )
 
-# exitCode=$?
-# echo ${exitcode}
-
 checkExitCode $? $E_CONNECTION_ERROR "$CONNECTION_TEST"
 
+# get oracle instance from connection string
+ORACLE_INSTANCE=${ORACLE_CONNECT_STRING##*@}
+ORACLE_INSTANCE_DIR=$SOURCES/$ORACLE_INSTANCE
+checkAndCreateDestination "$ORACLE_INSTANCE_DIR"
+
+# get users list for export schema scripts
 USER_LIST=$(sqlplus -silent /nolog <<SQL
 @$SCRIPT_DIR/src/login.sql ${ORACLE_CONNECT_STRING}
 @$SCRIPT_DIR/src/get_users.sql
@@ -63,3 +84,9 @@ SQL
 checkExitCode $? $E_CANT_GET_USERS "$USER_LIST"
 
 echo $USER_LIST
+
+if [ -z "${USER_LIST}" ]
+then
+    echo "Users not found!"
+    exit $E_USERS_NOT_FOUND
+fi
